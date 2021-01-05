031e6d62812833717cfcdda230b32b4ab70278b9
Steve Beaty <beatys@msudenver.edu>
2021-01-05 09:03:55 -0700

    A pretty good start to all three

--- dilbert.py
+++ dilbert.py
@@ -3,6 +3,7 @@ import os.path
 import urllib.request
 from html.parser import HTMLParser
 from datetime import date
+import imagesize
 
 # call only once a day
 
@@ -12,6 +13,9 @@ class MyHTMLParser(HTMLParser):
             for attr in attrs:
                 if attr[0] == 'property' and attr[1] == 'og:image':
                     urllib.request.urlretrieve(attrs[1][1], '/tmp/dilbert.gif')
+                    width, height = imagesize.get("/tmp/dilbert.gif")
+                    scale = width / 450
+                    print('${{image /tmp/dilbert.gif -s {}x{}}}'.format(int(width/scale), int(height/scale)))
                     sys.exit()
                             
 url = 'https://dilbert.com/strip/' + date.today().strftime("%Y-%m-%d")
--- nws.py
+++ nws.py
@@ -2,6 +2,7 @@
 
 import io
 import re
+import textwrap
 import os.path
 import urllib.request
 import xml.etree.ElementTree
@@ -48,26 +49,35 @@ def get_forecast(forecast):
 
     icons = get_forecast_conditions_icon(forecast_parameters)
     days = get_forecast_days(forecast)
-    words = get_worded_forecast(forecast_parameters)
+    words = get_forecast_weather(forecast_parameters)
     assert len(icons) == len(days) == len(words)
 
     zipped = zip(icons, days, words)
     # print(list(zipped))
+
+
+    count = 0
     for i in zipped:
-        print(get_icon(i[0]))
-        print(i[1].attrib['period-name'])
-        print(i[2].text)
+        offset = 100 if count % 2 else 0
+        row = int(count/2)
+        print("${{image {} -p {},{}}}".format(get_icon(i[0]), offset,
+            120 + row * 100, end=''))
+
+        print('${{goto 200}}{}: {}'.format(i[1].attrib['period-name'],
+            i[2].attrib["weather-summary"]), end='')
+
+        if count != 0 and count % 2:
+            print()
+            print('${{goto 200}}{}'.format('High/low'))
+            print('${voffset 10}')
+
+        count += 1
 
     # maxs = get_forecast_maximum_temperatures(forecast_parameters)
     # mins = get_forecast_minimum_temperatures(forecast_parameters)
     # maxs_and_mins = [j for i in zip(maxs,b) for j in i]
     # moreWeatherInformation = forecast.find('./moreWeatherInformation')
     # print('get_forecast_forecast', get_worded_forecast(forecast_parameters))
-
-    '''
-    both = zip(get_forecast_maximum_temperatures(forecast_parameters),
-        get_forecast_minimum_temperatures(forecast_parameters))
-    '''
 
 def get_current_location(current_parameters):
     location = current_parameters.find('./location')
@@ -104,37 +114,38 @@ def get_current(current):
     print('${{alignc}}{}'.format(
         get_current_weather(current_parameters)))
 
-    print('${{goto 100}}Temp:${{goto 200}}{}'.format(
+    print('Temp:${{goto 100}}{}'.format(
         get_current_temperature(current_parameters)), end='')
-    print('${{goto 350}}Speed:${{goto 450}}{}'.format(
+    print('${{goto 300}}Speed:${{goto 400}}{}'.format(
         get_current_wind_speed(current_parameters)), end='')
     print()
 
-    print('${{goto 100}}RH:${{goto 200}}{}'.format(
+    print('RH:${{goto 100}}{}'.format(
         get_current_humidity(current_parameters)), end='')
-    print('${{goto 350}}Direction:${{goto 450}}{}'.format(
+    print('${{goto 300}}Direction:${{goto 400}}{}'.format(
         get_current_wind_direction(current_parameters)), end='')
     print()
 
-    print('${{goto 100}}Pressure:${{goto 200}}{}'.format(
+    print('Pressure:${{goto 100}}{}'.format(
         get_current_pressure(current_parameters)), end='')
 
     print()
 
     filename = get_current_weather_icon(current_parameters)
-    print("${image ", filename, " -p 250,25}")
+    print("${{image {} -p 200,25}}".format(filename))
 
 
 # https://stackoverflow.com/questions/59067649/assert-true-vs-assert-is-not-none
-'''
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    tree = xml.etree.ElementTree.parse(io.BytesIO(html))
-'''
 
+'''
 with open('/home/stevebeaty/src/conky/weather.gov') as response:
     html = response.read()
     tree = xml.etree.ElementTree.parse(io.StringIO(html))
+'''
+
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    tree = xml.etree.ElementTree.parse(io.BytesIO(html))
     root = tree.getroot()
 
     forecast = root.find('./data[@type="forecast"]')
