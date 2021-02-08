a115812e27af6b7ef69beb21e9005bc6bb7a13a9
Steve Beaty <beatys@msudenver.edu>
2021-02-08 14:39:12 -0700

    Use the correct parameter to image

--- moon.sh
+++ moon.sh
@@ -5,8 +5,9 @@ curl -s -o /tmp/$$ https://www.moongiant.com/phase/today/
 image=$(grep 'div id="todayMoonContainer"' < /tmp/$$ | \
     sed -e 's/.*src="//' -e 's/".*//' -e 's,^,https://www.moongiant.com,')
 
-curl -s -o /tmp/moon.jpg "${image}"
-echo '${image /tmp/moon.jpg -f}'
+curl -s -o "/tmp/$$.jpg" "${image}"
+convert "/tmp/$$.jpg" -modulate 120 /tmp/moon.jpg
+echo '${image /tmp/moon.jpg -n}'
 
 age=$(grep "Moon Age" < /tmp/$$  | 
     sed -e 's/^  *//' -e 's,<span>,,g' -e 's,</span>,,g' -e 's,<br>,,')
